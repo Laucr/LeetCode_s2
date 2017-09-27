e14a55d12f60b508a26a79c4fdaea5a29041ad7d
Laucr <leo.lau.inami@gmail.com>
2017-09-27 10:18:07 +0800

    A

--- medium/remove_nth_from_end.py
+++ medium/remove_nth_from_end.py
@@ -11,23 +11,3 @@ def removeNthFromEnd(head, n):
     :type n: int
     :rtype: ListNode
     """
-    length = 0
-    this = ListNode(head.val)
-    this.next = head.next
-    while this.next:
-        length += 0
-        this = this.next
-    end = this
-    end.next = head
-    this.next = head.next
-    listnode = ListNode(None)
-    for i in range(length):
-        if i == length - 1:
-            listnode.val = this.next.val
-            listnode.next = this.next.next
-            this.next = this.next.next
-            break
-        this = this.next
-    return head
-
-removeNthFromEnd([1], 1)
