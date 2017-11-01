9fa6894e6cd7ea1f8e9e38ce27dd5ea91ff87e9c
Laucr <leo.lau.inami@gmail.com>
2017-11-01 17:13:07 +0800

    A

--- hard/remove_duplicate_letters.py
+++ hard/remove_duplicate_letters.py
@@ -1,5 +1,25 @@
-def removeDuplicateLetters(self, s):
+def removeDuplicateLetters(s):
     """
     :type s: str
     :rtype: str
     """
+    d = {}
+    for i in range(len(s)):
+        if s[i] not in d:
+            d[s[i]] = 1
+        else:
+            d[s[i]] += 1
+
+    stack = []
+    for c in s:
+        d[c] -= 1
+        if c in stack:
+            continue
+        while stack and stack[-1] > c and d[stack[-1]] > 0:
+            stack.pop()
+        stack.append(c)
+
+    return ''.join(stack)
+
+print removeDuplicateLetters("bcabc")
+
