2b28d098d758547dc2f48115d217df9b4164e1fc
Laucr <leo.lau.inami@gmail.com>
2017-11-02 19:04:32 +0800

    A.

--- hard/basic_calculator.py
+++ hard/basic_calculator.py
@@ -3,43 +3,30 @@ def calculate(s):
     :type s: str
     :rtype: int
     """
-    op = ''
-    operator = []
-    operands = []
-    for c in s:
-        if c == ' ':
-            continue
-        if c.isdigit():
-            op += c
-        else:
-            operator.append(c)
-            if op:
-                operands.append(op)
-            op = ''
-    if op:
-        operands.append(op)
-    sign = 1
+    result, num, sign = 0, 0, 1
     stack = []
-    print operands
-    print operator
-    for o in operator:
-        if o == '(':
-            if stack and stack[-1] == -1:
-                sign *= -1
-        elif o == ')':
-            sign = 1
+    for c in s:
+        if c.isdigit():
+            num = num * 10 + (ord(c) - ord('0'))
+        elif c == '+':
+            result += num * sign
+            num, sign = 0, 1
+        elif c == '-':
+            result += num * sign
+            num, sign = 0, -1
+        elif c == '(':
+            stack.append(result)
+            stack.append(sign)
+            result, num, sign = 0, 0, 1
+        elif c == ')':
+            result += num * sign
+            result *= stack.pop()
+            result += stack.pop()
+            num, sign = 0, 1
+    result += num * sign
+    return result
 
-        elif o == '+':
-            stack.append(sign * 1)
-        elif o == '-':
-            stack.append(sign * -1)
-    print stack
-    res = int(operands[0])
-    for i in range(1, len(operands)):
-        res += int(operands[i]) * stack[i - 1]
-    return res
 
-
-print calculate("(3-(2-(5-(9-(4)))))")
+print calculate("(1+(4+2)-3)+(6+8)")
 
 #
